830bf95a92d410a7be9b73797d92a9e2746e35c0
Aviv <aviv.dror@gmail.com>
2024-06-02 01:43:39 +0200

    remove unused logic

--- src/config.js
+++ src/config.js
@@ -1,3 +1,1 @@
-export const SW_API_BASE_URL = 'https://swapi.dev/api/';
-export const ST_API_BASE_URL = 'https://stapi.co/api/v2/rest/';
-export const HP_API_BASE_URL = 'https://api.potterdb.com/v1/';
+export const HP_API_BASE_URL = "https://api.potterdb.com/v1/";
